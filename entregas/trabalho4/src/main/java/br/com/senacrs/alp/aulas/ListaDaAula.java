23d56e89dc5177fa684fe5a4fcb429e6108167f6
631220012 <631220012@Sala603-18.fspoaeduc.com.br>
2013-03-19 21:46:18 -0300

    Conexion teste Work 4

---
+++ entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/ListaDaAula.java
@@ -0,0 +1,60 @@
+package br.com.senacrs.alp.aulas;
+
+public class ListaDaAula<T> 
+	implements Lista<T> {
+
+	@Override
+	public void adicionarFinal(T valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void adicionarInicio(T valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void adicionarPosicao(int posicao, T valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T obterPrimeiro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T obterUltimo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T obterPosicao(int posicao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int obterTamanho() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T removerPosicao(int posicao) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void esvaziar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/ListaFactory.java
+++ entregas/trabalho4/src/main/java/br/com/senacrs/alp/aulas/ListaFactory.java
@@ -11,7 +11,7 @@ public class ListaFactory {
 		
 		Lista<T> resultado = null;
 		
-		//implementar
+		resultado = new ListaDaAula<T>();
 		
 		return resultado;
 	}
